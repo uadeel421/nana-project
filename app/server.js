7a16949a4337ef10475b9ceaf499692cee294120
uadeel <mua.bht@gmail.com>
2025-03-24 10:09:28 +0500

    2nd AZK8s Change

--- app/server.js
+++ app/server.js
@@ -4,16 +4,16 @@ let fs = require('fs');
 let MongoClient = require('mongodb').MongoClient;
 let app = express();
 
-// bodyParser was deprecated: https://stackoverflow.com/a/59892173
+// bodyParser was deprecated: https://stackoverflow.com/a/59892173S
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));S
   });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-2.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
